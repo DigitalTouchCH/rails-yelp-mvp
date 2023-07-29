5c3447f5a8ef79168ae00e0cafbc486174547f0a
thomas murat <thomas.murat974@gmail.com>
2023-07-29 11:50:47 +0200

    correction validation phone number and seed restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,6 @@
 class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
-  validates :phone_number, presence: true
   VALID_CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
   validates :category, inclusion: { in: VALID_CATEGORIES }
   has_many :reviews, dependent: :destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destroy_all
+
+restaurants_attributes = [
+  {
+    name: "Epicure",
+    address: "75008 Paris",
+    phone_number: "01 43 54 23 31",
+    category: "french"
+  },
+  {
+    name: "Sushi Master",
+    address: "123 Rue de Tokyo, 75013 Paris",
+    phone_number: "01 56 78 90 12",
+    category: "japanese"
+  },
+  {
+    name: "Pasta Paradise",
+    address: "456 Via Roma, 00100 Rome",
+    phone_number: "+39 06 78 90 123",
+    category: "italian"
+  },
+  {
+    name: "Great Wall",
+    address: "789 Nanjing Road, Shanghai",
+    phone_number: "+86 21 45 67 89 01",
+    category: "chinese"
+  },
+  {
+    name: "Belgian Delight",
+    address: "10 Grand-Place, Brussels",
+    phone_number: "+32 2 34 56 78 90",
+    category: "belgian"
+  }
+]
+
+restaurants_attributes.each do |attributes|
+  Restaurant.create!(attributes)
+end
