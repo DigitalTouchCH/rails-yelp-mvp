b1ca361d95513d39c0fe5568bf1b90da6c62b3b6
thomas murat <thomas.murat974@gmail.com>
2023-07-29 11:39:18 +0200

    model dependencies and validation data

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  VALID_CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :category, inclusion: { in: VALID_CATEGORIES }
+  has_many :reviews, dependent: :destroy
 end
